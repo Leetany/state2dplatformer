0f11a3d9ed784da2135c2943aafba9dba349cf95
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-04-03 20:28:44 +0900

    state pattern study1~2

---
+++ 2DStatemachine_Learn/Assets/Script/Player.cs
@@ -0,0 +1,174 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    [Header("대쉬 정보")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
+    [Header("공격 정보")]
+    [SerializeField]private float comboTime = 0.3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int comboCounter;
+
+
+
+    private float xInput;
+
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+
+    [Header("Collsion info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    
+    void Update()
+    {
+        CheckInput();
+        Movement();
+        CollisionChecks();
+
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+
+        AnimatorControllers();
+        FlipController();
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+        comboCounter++;
+
+        if (comboCounter > 2)
+            comboCounter = 0;
+
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+    }
+
+    private void StartAttackEvent()
+    {
+        if (comboTimeWindow < 0)
+            comboCounter = 0;
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer <0 && !isAttacking)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
+    }
+
+    private void Movement()
+    {
+        if(dashTime > 0)
+        {
+            rb.linearVelocity = new Vector2(xInput * dashSpeed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if(isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.linearVelocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if(rb.linearVelocityX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(rb.linearVelocityX <0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+}
---
+++ 2DStatemachine_Learn/Assets/Script/PlayerAnimEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private Player player;
+
+    void Start()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    
+    public void AnimationTrigger()
+    {
+        player.AttackOver();
+    }
+}
